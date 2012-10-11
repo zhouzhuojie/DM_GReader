d1bc6804320afbf08bcc4b6ee6216d81e3e0dc2f
zhouzhuojie <zhouzhuojie@gmail.com>
2012-10-11 19:02:50 -0400

    Add timestamp and clustering feature
    1. Only crawl one day's
    data.
    2. Clustering the documents into k clusters.
    3. Temporarily use arbitrary article in each cluster to represent them.

--- DM_GReader.py
+++ DM_GReader.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # *-* coding: UTF-8 *-*
+import datetime, calendar
+import random
+import requests
 from libgreader import GoogleReader, ClientAuthMethod
 from pattern.vector import Document, Corpus, LEMMA
 from bs4 import BeautifulSoup
@@ -16,7 +19,7 @@ class DM_GReader():
         self.categories = self.reader.getCategories()
         self.corpus = Corpus()
 
-    def import_category(self, category_id=3, cont=200, path=None, local=False):
+    def import_category(self, category_id=3, path=None, local=False):
         """Import the specific category to a Pattern Corpus for future calculation.
         category_id: the integer indicates which category to use.
         cont: the integer tells how many queries to issue to continuously crawl the GReader.
@@ -31,15 +34,22 @@ class DM_GReader():
             self.corpus = Corpus.load(path)
             return
 
-
         self.target_category = self.categories[category_id]
         continuation = None
 
-        for i in range(cont):
-            print i, '/', cont
+        # Crawl only the data within one day
+        yesterday = calendar.timegm((datetime.date.today()-datetime.timedelta(days=1)).timetuple())
+
+        i = 1
+
+        while 1:
+
             self.target_category_content = self.reader.getCategoryContent(self.target_category, continuation=continuation)
             feeds = self.target_category_content[u'items']
-            print len(feeds)
+
+            if self.target_category_content['updated'] < yesterday:
+                break
+
             feeds_docs = []
             for feed in feeds:
                 doc_name = feed[u'id'][-16:]
@@ -58,4 +68,39 @@ class DM_GReader():
                 print 'Finished!'
                 break
 
+            print i
+            i = i + 1
+
         self.corpus.save(path, update=True)
+
+    def _generate_clusters(self, k=10, p=1.0):
+        """Use KMEANS method by default, and choose the initial k values by KMPP method.
+        k is the number of clusters.
+        p is to control the error of KMEANS, when p=1.0 is faster with small error.
+        """
+        from pattern.vector import KMEANS, KMPP
+        self.clusters = self.corpus.cluster(method=KMEANS, k=10, seed=KMPP, p=1.0)
+
+    def generate_repr_ids(self, k):
+        """
+        For each cluster, we choose an arbitary article as the cluster's representative.
+
+        Return the ids of the article, here the document name is the article's id.
+        Google Reader is using "i=http://www.google.com/reader/api/0/stream/items/contents" to get the content of a specific data.
+
+        TODO How to generate representative articles, here we can only generate an arbitrary article from each cluster. Consider the centrol of the cluster?
+        """
+        self._generate_clusters(k=k)
+        ids = [random.choice(i).name for i in self.clusters]
+        return ids
+
+    def get_article_content(self, ids):
+        """docstring for get_article_content"""
+        url = 'http://www.google.com/reader/api/0/stream/items/contents'
+        id_handle = 'tag:google.com,2005:reader/item/%s'
+
+        contents = []
+        for _id in ids:
+            r = requests.post(url, data={'i':(id_handle %_id)})
+            contents.append(r.json)
+        return contents
